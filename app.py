7efd941ed720f50d4669a0a3bd2741b8e89ee380
Колесников Дмитрий <Kolesn2003@yandex.ru>
2023-09-15 22:40:35 +0700

    Вывод страницы первой лабораторной

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def start():
-    return "web-сервер на flusk"
+    return """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Колесников Дмитрий Константинович, лабораторная 1</title>
+    </head>
+    <body>
+        <header>
+            НГТУ, ФБ, Лабораторная работа 1
+        </header>
+
+        <h1>web-сервер на flask</h1>
+
+        <footer>
+            &copy: Колесников Дмитрий, ФБИ-12, 3 курс, 2023
+        </footer>
+    </body>
+</html>
+"""
