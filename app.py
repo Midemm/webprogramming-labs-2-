afd0ebf9500f4ec4537d73b9601d2af511be1a9a
Колесников Дмитрий <Kolesn2003@yandex.ru>
2023-09-29 13:45:20 +0700

    Книги

--- app.py
+++ app.py
@@ -179,4 +179,16 @@ def example():
         {'name': 'Мандарины', 'price': 95},
         {'name': 'Манго', 'price': 321}
         ]
-    return render_template('example.html', name=name, lab_namber=lab_namber, group=group, kurs=kurs, fruits=fruits)
+    books = [
+        {'autor': 'Маргарет Митчелл' , 'name_book': 'Унесенные ветром' , 'genre': 'Исторический роман' , 'pages': 992},
+        {'autor': 'Стивен Кинг' , 'name_book': '11/22/63' , 'genre': ' Научная фантастика' , 'pages': 800},
+        {'autor': 'Ной Гордон' , 'name_book': 'Лекарь. Ученик Авиценны' , 'genre': 'Исторические приключения' , 'pages': 816},
+        {'autor': 'Джон Стейнбек' , 'name_book': 'К востоку от Эдема' , 'genre': 'Классическая проза' , 'pages': 768},
+        {'autor': 'Джордж Элиот' , 'name_book': 'Мидлмарч' , 'genre': 'Зарубежная классика' , 'pages': 832},
+        {'autor': 'Джордж Оруэлл' , 'name_book': '1984' , 'genre': 'Фантастика' , 'pages': 328},
+        {'autor': 'Михаил Булгаков' , 'name_book': 'Мастер и Маргарита' , 'genre': 'Роман' , 'pages': 384},
+        {'autor': 'Фёдор Достоевский' , 'name_book': 'Преступление и наказание' , 'genre': 'Роман' , 'pages': 560},
+        {'autor': 'Дж. К. Роулинг' , 'name_book': 'Гарри Поттер и философский камень' , 'genre': 'Фэнтези' , 'pages': 309},
+        {'autor': 'Антуан де Сент-Экзюпери' , 'name_book': 'Маленький принц' , 'genre': 'Философская притча' , 'pages': 96}
+    ]
+    return render_template('example.html', name=name, lab_namber=lab_namber, group=group, kurs=kurs, fruits=fruits, books=books)
