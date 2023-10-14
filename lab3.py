303d4bfe327c612c7bac4812c7478b063fe61157
Колесников Дмитрий <Kolesn2003@yandex.ru>
2023-10-14 10:25:57 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -6,6 +6,7 @@ lab3 = Blueprint('lab3',__name__)
 def lab():
     return render_template('lab3.html')
 
+
 @lab3.route('/lab3/form1/')
 def form1():
     errors = {}
@@ -18,9 +19,11 @@ def form1():
     sex = request.args.get('sex')
     return render_template('/form1.html', user=user, sex=sex, age=age, errors=errors)
 
+
 @lab3.route('/lab3/order/')
 def order():
     return render_template('/order.html')
+
 
 @lab3.route('/lab3/pay/')
 def pay():
@@ -39,6 +42,39 @@ def pay():
         price += 10
     return render_template('/pay.html', price=price)
 
+
 @lab3.route('/lab3/success/')
 def success():
     return render_template('/success.html')
+
+
+@lab3.route('/lab3/form2/')
+def form2():
+    errors = {}
+    user = request.args.get('user')
+    if user == '':
+        errors['user'] = 'Заполните поле!'
+        
+    bilet_type = request.args.get('bilet_type')
+
+    mesto = request.args.get('mesto')
+
+    bagazh = request.args.get('bagazh')
+
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+
+    Punkt_A = request.args.get('Punkt_A')
+    if Punkt_A == '':
+        errors['Punkt_A'] = 'Заполните поле!'
+
+    Punkt_B = request.args.get('Punkt_B')
+    if Punkt_B == '':
+        errors['Punkt_B'] = 'Заполните поле!'
+
+    data = request.args.get('data')
+    if data == ' - - ':
+        errors['data'] = 'Заполните поле!'
+    return render_template('/form2.html', user=user, bilet_type=bilet_type, 
+    mesto=mesto, bagazh=bagazh, age=age, Punkt_A=Punkt_A, Punkt_B=Punkt_B, data=data, errors=errors)
