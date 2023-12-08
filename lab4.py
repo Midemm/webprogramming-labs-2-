7a42c43500bbf29d98b122b23daa089b25d19039
Колесников Дмитрий <Kolesn2003@yandex.ru>
2023-12-08 23:03:25 +0700

    Куки — цвет текста

--- lab4.py
+++ lab4.py
@@ -97,3 +97,16 @@ def zerno():
 @lab4.route('/lab4/formzerno')
 def formazerna():
     return render_template('formzerno.html')
+
+@lab4.route('/lab4/cookies', methods=['GET', 'POST'])
+def cookies():
+    if request.method == 'GET':
+        return render_template('cookies.html')
+
+    color = request.form.get('color')
+
+    headers = {
+      'Set-Cookie': 'color=' + color + '; path=/',
+      'Location': '/lab4/cookies'
+    }
+    return '', 303, headers
